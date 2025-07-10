650cc3cfafe6ae4a2543d799614a017af1398898
Avijit Tarafder <avijittarafder22@gmail.com>
2025-07-10 22:17:27 +0600

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Float
+from database import Base
+
+class Vehicle(Base):
+    __tablename__ = "vehicles"
+
+    vehicle_id = Column(String, primary_key=True, index=True)
+    vehicle_type = Column(String, nullable=False)
+    cost = Column(Float, nullable=False)
+    premium_amount = Column(Float, nullable=True)
