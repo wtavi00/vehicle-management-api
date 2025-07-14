a7707f406727bf58b65115050e189184d2338d7f
Avijit Tarafder <avijittarafder22@gmail.com>
2025-07-14 14:45:44 +0600

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
