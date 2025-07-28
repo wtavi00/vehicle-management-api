2c9a25fb562f74e9d1e4cec7f27654a07bf39a80
Avijit Tarafder <avijittarafder22@gmail.com>
2025-07-28 10:22:28 +0600

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -4,3 +4,10 @@ from typing import Optional
 class VehicleBase(BaseModel):
     vehicle_type: str
     cost: float
+
+    @validator("vehicle_type") 
+    def validate_type(cls, v):
+        if v not in ["Two Wheeler", "Four Wheeler"]:
+            raise ValueError("Vehicle type must be 'Two Wheeler' or 'Four Wheeler'")
+        return v
+
