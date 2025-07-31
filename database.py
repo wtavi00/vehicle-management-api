18de6fb5745a1c52720d755cb4cb4acdf9233176
Avijit Tarafder <avijittarafder22@gmail.com>
2025-07-31 15:37:47 +0600

    Update database.py

--- database.py
+++ database.py
@@ -6,3 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
