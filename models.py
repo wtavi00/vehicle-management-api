08c91b4dfcdc4201f004e955183043d0d4972d26
Avijit Tarafder <avijittarafder22@gmail.com>
2025-08-06 09:11:10 +0600

    Update models.py

--- models.py
+++ models.py
@@ -3,8 +3,9 @@ from database import Base
 
 class Vehicle(Base):
     __tablename__ = "vehicles"
-    
+
     vehicle_id = Column(String, primary_key=True, index=True)
     vehicle_type = Column(String, nullable=False)
     cost = Column(Float, nullable=False)
     premium_amount = Column(Float, nullable=True)
+    
